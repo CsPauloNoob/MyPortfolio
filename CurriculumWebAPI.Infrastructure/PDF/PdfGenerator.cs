273884ef87156a219ef748af5a27e4c48450cfb6
CsPauloNoob <ps616131@gmail.com>
2023-08-24 22:51:30 -0500

    adicionado pacote PDF

--- CurriculumWebAPI.Infrastructure/PDF/PdfGenerator.cs
+++ CurriculumWebAPI.Infrastructure/PDF/PdfGenerator.cs
@@ -1,13 +1,6 @@
 ﻿using CurriculumWebAPI.Domain.Models;
-using iTextSharp.text.pdf;
-using iTextSharp.text;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using CurriculumWebAPI.Domain.Interfaces;
-using iTextSharp.text.pdf.parser;
+using UglyToad.PdfPig;
 
 namespace CurriculumWebAPI.Infrastructure.PDF
 {
@@ -16,51 +9,7 @@ namespace CurriculumWebAPI.Infrastructure.PDF
         public async Task<string> Create(Curriculum curriculum)
         {
             
-            string pdfName = Guid.NewGuid().ToString()+".pdf";
-            string pdfFile = DirectoryManager.PdfPath(pdfName);
-
-            Document doc = new Document();
-
-            // Criar um escritor de PDF
-            PdfWriter writer = PdfWriter.GetInstance(doc, new FileStream(pdfFile, FileMode.Create));
-
-            // Abrir o documento para escrita
-            doc.Open();
-
-            CurriculumTop(doc, curriculum);
-
-            /*
-            // Adicionar linhas personalizadas
-            PdfContentByte contentByte = writer.DirectContent;
-            contentByte.SetLineWidth(1); // Espessura da linha
-
-            contentByte.MoveTo(50, 700); // Posição inicial da linha
-            contentByte.LineTo(550, 700); // Posição final da linha
-            contentByte.Stroke(); // Desenhar a linha
-
-            // Fechar o documento*/
-            doc.Close();
-
-            return pdfFile;
-        }
-
-        void CurriculumTop(Document doc, Curriculum curriculum)
-        {
-            //Fontes
-            Font nameFont = FontFactory.GetFont(FontFactory.HELVETICA_BOLD, 24);
-            Font secondTitle = FontFactory.GetFont(FontFactory.HELVETICA_BOLD, 18);
-            Font infoFontUnderLine = FontFactory.GetFont(FontFactory.HELVETICA, 12, Font.UNDERLINE);
-            Font infoFont = FontFactory.GetFont(FontFactory.HELVETICA, 12);
-
-
-            doc.Add(new Paragraph(curriculum.Nome, nameFont));
-            doc.Add(new Paragraph("Desenvolvedor Back-End Web .NET", infoFontUnderLine));
-
-            doc.Add(new Paragraph("\n"));
-
-            doc.Add(new Paragraph("Contato", secondTitle));
-            //doc.Add(new Paragraph(curriculum.Email, infoFont));
-            
+            return "";
         }
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # MyPortfolio
+ 
+  API para gerenciar dados de currículos.  :page_facing_up:
+
+ > Autenticação via Token JWT
+ > Formatação dos dados do curriculo do usuário em um curriculo *PDF*
+
+ :rocket: Como rodar em sua máquina
+
+Instale o dotnet 6 ou superior e execute o comando 
+> dotnet build
+
+e
+
+> dotnet run
+
